8382980317fc053c8c1439791cfd4c1f9ad79d00
Jamie Wilkinson <jamie@jamiedubs.com>
2012-01-27 02:09:04 -0500

    Fix uploads and add curl samples

--- app/controllers/scratches_controller.rb
+++ app/controllers/scratches_controller.rb
@@ -21,19 +21,24 @@ class ScratchesController < ApplicationController
 
   def create
     @scratch = Scratch.new
-    @scratch.data = params[:data]
+
+    if params[:data] && params[:data].kind_of?(String)
+      @scratch.data = params[:data]
+    elsif params[:data] && params[:data].kind_of?(ActionDispatch::Http::UploadedFile)
+      @scratch.data = params[:data].read
+    end
     @scratch.application = params[:application]
     @scratch.author = params[:author]
 
     respond_to do |format|
       if @scratch.save
-        format.html { render :text => "OK" }
-        format.xml { render :xml => {:success => false, :id => @scratch.id } }
-        format.json { render :json => {:success => false, :id => @scratch.id } }
+        format.html { render :text => "OK", :layout => false }
+        format.xml { render :xml => {:success => true, :id => @scratch.id } }
+        format.json { render :json => {:success => true, :id => @scratch.id } }
       else
         errors = @scratch.errors.map{|k,v| v.blank? ? nil : "#{k} #{v}" }.compact
         logger.warn "errors => #{errors.inspect}"
-        format.html { render :text => "Failed: #{errors.join(',')}", :status => 400 }
+        format.html { render :text => "Failed: #{errors.join(',')}", :layout => false, :status => 400 }
         format.xml { render :xml => {:success => false, :errors => errors}, :status => 400 }
         format.json { render :json => {:success => false, :errors => errors}, :status => 400 }
       end
